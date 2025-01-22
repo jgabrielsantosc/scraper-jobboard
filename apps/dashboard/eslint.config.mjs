a3a400e1ed994c32fe7e6ac3ab4e83cf027fa3e1
João Gabriel Santos <jgabrielsantosc@gmail.com>
2025-01-22 10:42:07 -0300

    ajuste nextjs

--- apps/dashboard/eslint.config.mjs
+++ apps/dashboard/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import nextPlugin from '@next/eslint-plugin-next'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      '@next/next': nextPlugin
+    },
+    extends: [
+      'next/core-web-vitals'
+    ]
+  }
 ];
 
 export default eslintConfig;
