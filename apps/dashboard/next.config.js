58b1f96c51fa87b86954a85ab60a3f0e2f2d4257
João Gabriel Santos <jgabrielsantosc@gmail.com>
2025-01-22 11:18:59 -0300

    rollback versao react

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -3,13 +3,7 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     outputFileTracingRoot: require('path').join(__dirname, '../../'),
-  },
-  transpilePackages: [
-    '@radix-ui/react-dialog',
-    '@radix-ui/react-dropdown-menu',
-    '@radix-ui/react-slot',
-    '@radix-ui/react-toast'
-  ]
+  }
 }
 
 module.exports = nextConfig 
